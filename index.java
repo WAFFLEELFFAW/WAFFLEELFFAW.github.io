c1b4734256c5939773822ace090830666aaf17d7
WAFFLEELFFAW <122127972+WAFFLEELFFAW@users.noreply.github.com>
2023-01-07 07:50:08 +0900

    Create index.java

---
+++ index.java
@@ -0,0 +1,27 @@
+<script>
+function calc1(){	total=0;	total=	13960	*	document.form1.hinmoku1.value   /1000;	document.form1.ans1.value=total;	}			
+function calc2(){	total=0;	total=	4291	*	document.form1.hinmoku2.value	/1000;	document.form1.ans2.value=total;	}			
+function calc3(){	total=0;	total=	8300	*	document.form1.hinmoku3.value	/1000;	document.form1.ans3.value=total;	}			
+function calc4(){	total=0;	total=	66	*	document.form1.hinmoku4.value	/1000;	document.form1.ans4.value=total;	}
+function calc5(){	total=0;	total=	22926	*	document.form1.hinmoku5.value	/1000;	document.form1.ans5.value=total;	}
+function calc6(){	total=0;	total=	13637	*	document.form1.hinmoku6.value	/1000;	document.form1.ans6.value=total;	}
+function calc7(){	total=0;	total=	108	*	document.form1.hinmoku7.value	/1000;	document.form1.ans7.value=total;	}
+
+
+function calc100(){	total=0;	total=	7046	*	document.form1.hinmoku100.value   /1000;	document.form1.ans100.value=total;	}
+
+
+function totalwf(){document.form1.goukei.value=
+eval(document.form1.ans1.value)+
+eval(document.form1.ans2.value)+
+eval(document.form1.ans3.value)+
+eval(document.form1.ans4.value)+
+eval(document.form1.ans5.value)+
+eval(document.form1.ans6.value)+
+eval(document.form1.ans7.value)+
+eval(document.form1.ans100.value)
+
+
+
+;}
+</script>
