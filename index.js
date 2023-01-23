920d6d9171eae9fbf52231dc7e30a0d547e49058
WAFFLEELFFAW <122127972+WAFFLEELFFAW@users.noreply.github.com>
2023-01-23 16:29:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ function calc2() { total = 0; total = 4291 * stringToNum(document.form1.hinmoku2
 function calc3() { total = 0; total = 8300 * stringToNum(document.form1.hinmoku3.value) / 1000; document.form1.ans3.value = total; }
 function calc4() { total = 0; total = 66 * stringToNum(document.form1.hinmoku4.value) / 1000; document.form1.ans4.value = total; }
 function calc5() { total = 0; total = 22926 * stringToNum(document.form1.hinmoku5.value) / 1000; document.form1.ans5.value = total; }
-function calc6() { total = 0; total = 13637 * stringToNum(document.form1.hinmoku6.value) / 1000; document.form1.ans6.value = total; }
-function calc7() { total = 0; total = 108 * stringToNum(document.form1.hinmoku7.value) / 1000; document.form1.ans7.value = total; }
+function calc6() { total = 0; total = 7050 * stringToNum(document.form1.hinmoku6.value) / 1000; document.form1.ans6.value = total; }
+function calc7() { total = 0; total = 13637 * stringToNum(document.form1.hinmoku7.value) / 1000; document.form1.ans7.value = total; }
 function calc8() { total = 0; total = 108 * stringToNum(document.form1.hinmoku8.value) / 1000; document.form1.ans8.value = total; }
-function calc9() { total = 0; total = 108 * stringToNum(document.form1.hinmoku9.value) / 1000; document.form1.ans9.value = total; }
-function calc10() { total = 0; total = 108 * stringToNum(document.form1.hinmoku10.value) / 1000; document.form1.ans10.value = total; }
-function calc11() { total = 0; total = 108 * stringToNum(document.form1.hinmoku11.value) / 1000; document.form1.ans11.value = total; }
+function calc9() { total = 0; total = 62798 * stringToNum(document.form1.hinmoku9.value) / 1000; document.form1.ans9.value = total; }
+function calc10() { total = 0; total = 19310 * stringToNum(document.form1.hinmoku10.value) / 1000; document.form1.ans10.value = total; }
+function calc11() { total = 0; total = 37353 * stringToNum(document.form1.hinmoku11.value) / 1000; document.form1.ans11.value = total; }
 function calc12() { total = 0; total = 108 * stringToNum(document.form1.hinmoku12.value) / 1000; document.form1.ans12.value = total; }
 function calc13() { total = 0; total = 108 * stringToNum(document.form1.hinmoku13.value) / 1000; document.form1.ans13.value = total; }
 function calc14() { total = 0; total = 108 * stringToNum(document.form1.hinmoku14.value) / 1000; document.form1.ans14.value = total; }
