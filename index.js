72d99ce58bf79e8ebc13aa99b4828918178b363e
TOKUSHI-2000 <oz_sk_as_2002@outlook.jp>
2023-02-06 23:34:34 +0900

    繰り返し計算する処理の最適化

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ const waterCoefficient =
     ]
 
 //行のINPUTタグ要素数
-const numOfelementOfLine =2;
+const numOfelementOfLine = 2;
 //列のINPUTタグ要素数
-const numOfelementOfRow =15;
+const numOfelementOfRow = 15;
 
 //文字列を数字にする
 function stringToNum(x) {
@@ -35,16 +35,16 @@ function stringToNum(x) {
 }
 
 function calctest(numString) {
-    const num =Number(numString.substr(7));
-    console.log(numString, num, document.form1["ans"+num].value);
-    document.form1["ans"+num].value = waterCoefficient[num-1] * stringToNum(document.form1["hinmoku"+num].value) / 1000;
+    const num = Number(numString.substr(7));
+    console.log(numString, num, document.form1["ans" + num].value);
+    document.form1["ans" + num].value = waterCoefficient[num - 1] * stringToNum(document.form1["hinmoku" + num].value) / 1000;
 }
 
-function calc100() {let total = 0; total = 7050 * document.form1.hinmoku100.value / 1000; document.form1.ans100.value = total; }
+function calc100() { let total = 0; total = 7050 * document.form1.hinmoku100.value / 1000; document.form1.ans100.value = total; }
 
 
 function totalwf() {
-    document.getElementById('goukei').textContent =
+    const m =
         stringToNum(document.form1.ans1.value) +
         stringToNum(document.form1.ans2.value) +
         stringToNum(document.form1.ans3.value) +
@@ -61,48 +61,21 @@ function totalwf() {
         stringToNum(document.form1.ans14.value) +
         stringToNum(document.form1.ans15.value) +
         stringToNum(document.form1.ans100.value);
+
+    //合計
+    document.getElementById('goukei').textContent = m;
     var goukei = document.getElementById('goukei');
     goukei.insertAdjacentHTML("beforeend", "(L)");
-    var pet_m =
-        stringToNum(document.form1.ans1.value) +
-        stringToNum(document.form1.ans2.value) +
-        stringToNum(document.form1.ans3.value) +
-        stringToNum(document.form1.ans4.value) +
-        stringToNum(document.form1.ans5.value) +
-        stringToNum(document.form1.ans6.value) +
-        stringToNum(document.form1.ans7.value) +
-        stringToNum(document.form1.ans8.value) +
-        stringToNum(document.form1.ans9.value) +
-        stringToNum(document.form1.ans10.value) +
-        stringToNum(document.form1.ans11.value) +
-        stringToNum(document.form1.ans12.value) +
-        stringToNum(document.form1.ans13.value) +
-        stringToNum(document.form1.ans14.value) +
-        stringToNum(document.form1.ans15.value) +
-        stringToNum(document.form1.ans100.value);
-    var pet = pet_m / 2;
+
+    //ペットボトル
+    var pet = m / 2;
     var pet = Math.round(pet)
     document.getElementById('pet').textContent = (pet)
     var pet = document.getElementById('pet');
     pet.insertAdjacentHTML("beforeend", "(本)");
-    var bath_m =
-        stringToNum(document.form1.ans1.value) +
-        stringToNum(document.form1.ans2.value) +
-        stringToNum(document.form1.ans3.value) +
-        stringToNum(document.form1.ans4.value) +
-        stringToNum(document.form1.ans5.value) +
-        stringToNum(document.form1.ans6.value) +
-        stringToNum(document.form1.ans7.value) +
-        stringToNum(document.form1.ans8.value) +
-        stringToNum(document.form1.ans9.value) +
-        stringToNum(document.form1.ans10.value) +
-        stringToNum(document.form1.ans11.value) +
-        stringToNum(document.form1.ans12.value) +
-        stringToNum(document.form1.ans13.value) +
-        stringToNum(document.form1.ans14.value) +
-        stringToNum(document.form1.ans15.value) +
-        stringToNum(document.form1.ans100.value);
-    var bath = bath_m / 200;
+
+    //風呂
+    var bath = m / 200;
     var bath = Math.round(bath)
     document.getElementById('bath').textContent = (bath)
     var bath = document.getElementById('bath');
